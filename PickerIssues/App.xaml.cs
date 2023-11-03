fafb032c9c3578e6dea36661c88233df5fc352ed
borrmann <max.borrmann@gmx.net>
2023-11-03 18:26:45 +0000

    Add project files.

---
+++ PickerIssues/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PickerIssues
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PickerIssues/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace PickerIssues
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            // now lets pick a photo
+            PickOptions options = new()
+            {
+                PickerTitle = "Pick multiple files.... or not?",
+                FileTypes = FilePickerFileType.Images
+            };
+
+            var fileResult = await FilePicker.Default.PickMultipleAsync(options);
+
+            CounterBtn.Text = $"{fileResult?.Count() ?? 0} files picked";
+        }
+    }
+}
